3f65700044e0d1f156c538e4e472470a5c422b42
Glychee <haibso@hotmail.com>
2024-03-08 00:07:33 +0100

    Added new hardware revision tests

---
+++ CoinDispenser/DispenseOnClick/DispenseOnClick.ino
@@ -0,0 +1,37 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+int minpos = 10;
+int maxpos = 85;
+
+void setup() {
+  myservo.write(minpos); 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(6,INPUT);
+}
+
+void loop() {
+  while(digitalRead(6)){};
+  for (pos = minpos; pos <= maxpos; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  
+  for (pos = maxpos; pos >= minpos; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
